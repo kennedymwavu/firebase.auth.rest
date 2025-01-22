e534cda9dba10cd58c54a9e0d801a6e60e44c59a
Kennedy Mwavu <mwavukennedy@gmail.com>
2025-01-22 22:22:53 +0300

    ch: avoid usage of the pipe operator

--- R/update_profile.R
+++ R/update_profile.R
@@ -46,13 +46,15 @@ update_profile <- function(
     photo_url = NULL,
     delete_attribute = NULL) {
   url_path <- "/v1/accounts:update"
+
   data <- list(
     idToken = id_token,
     displayName = display_name,
     photoUrl = photo_url,
     deleteAttribute = delete_attribute,
     returnSecureToken = TRUE
-  ) |>
-    Filter(f = Negate(is.null))
+  )
+  data <- Filter(f = Negate(is.null), x = data)
+
   perform_req(url_path = url_path, data = data)
 }
